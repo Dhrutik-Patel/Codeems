ac88d9818f12baab1013abcb42044a228fc32a02
Dhrutik-Patel <dhrutikpatel2017@gmail.com>
2024-02-03 16:35:14 +0530

    [IMP] frontend: orders

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,57 +1,59 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 import {
-    createBrowserRouter,
-    createRoutesFromElements,
-    Route,
-    RouterProvider,
-} from 'react-router-dom';
-import { Provider } from 'react-redux';
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import { Provider } from "react-redux";
 // import 'bootstrap/dist/css/bootstrap.min.css';
-import './assets/styles/bootstrap.custom.css';
-import './assets/styles/index.css';
-import App from './App';
-import HomePage from './pages/HomePage';
-import ProductPage from './pages/ProductPage';
-import store from './store';
-import CartPage from './pages/CartPage';
-import LoginPage from './pages/LoginPage';
-import RegisterPage from './pages/RegisterPage';
-import ShippingPage from './pages/ShippingPage';
-import PrivateRoute from './components/PrivateRoute';
-import PaymentPage from './pages/PaymentPage';
-import PlaceOrder from './pages/PlaceOrder';
-import OrderPage from './pages/OrderPage';
-import { PayPalScriptProvider } from '@paypal/react-paypal-js';
+import "./assets/styles/bootstrap.custom.css";
+import "./assets/styles/index.css";
+import App from "./App";
+import HomePage from "./pages/HomePage";
+import ProductPage from "./pages/ProductPage";
+import store from "./store";
+import CartPage from "./pages/CartPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import ShippingPage from "./pages/ShippingPage";
+import PrivateRoute from "./components/PrivateRoute";
+import PaymentPage from "./pages/PaymentPage";
+import PlaceOrder from "./pages/PlaceOrder";
+import OrderPage from "./pages/OrderPage";
+import { PayPalScriptProvider } from "@paypal/react-paypal-js";
+import ProfilePage from "./pages/ProfilePage";
 
 const routes = createBrowserRouter(
-    createRoutesFromElements(
-        <Route path='/' element={<App />}>
-            <Route index={true} path='/' element={<HomePage />} />
-            <Route path='/product/:id' element={<ProductPage />} />
-            <Route path='/cart' element={<CartPage />} />
-            <Route path='/login' element={<LoginPage />} />
-            <Route path='/register' element={<RegisterPage />} />
+  createRoutesFromElements(
+    <Route path='/' element={<App />}>
+      <Route index={true} path='/' element={<HomePage />} />
+      <Route path='/product/:id' element={<ProductPage />} />
+      <Route path='/cart' element={<CartPage />} />
+      <Route path='/login' element={<LoginPage />} />
+      <Route path='/register' element={<RegisterPage />} />
 
-            <Route path='' element={<PrivateRoute />}>
-                <Route path='/shipping' element={<ShippingPage />} />
-                <Route path='/payment' element={<PaymentPage />} />
-                <Route path='/placeorder' element={<PlaceOrder />} />
-                <Route path='/order/:id' element={<OrderPage />} />
-            </Route>
+      <Route path='' element={<PrivateRoute />}>
+        <Route path='/shipping' element={<ShippingPage />} />
+        <Route path='/payment' element={<PaymentPage />} />
+        <Route path='/placeorder' element={<PlaceOrder />} />
+        <Route path='/order/:id' element={<OrderPage />} />
+        <Route path='/profile' element={<ProfilePage />} />
+      </Route>
 
-            <Route path='*' element={<h1>Not Found</h1>} />
-        </Route>
-    )
+      <Route path='*' element={<h1>Not Found</h1>} />
+    </Route>
+  )
 );
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <React.StrictMode>
-        <Provider store={store}>
-            <PayPalScriptProvider deferLoading={true}>
-                <RouterProvider router={routes} />
-            </PayPalScriptProvider>
-        </Provider>
-    </React.StrictMode>
+  <React.StrictMode>
+    <Provider store={store}>
+      <PayPalScriptProvider deferLoading={true}>
+        <RouterProvider router={routes} />
+      </PayPalScriptProvider>
+    </Provider>
+  </React.StrictMode>
 );
--- frontend/src/slices/usersApiSlice.js
+++ frontend/src/slices/usersApiSlice.js
@@ -1,35 +1,48 @@
-import { USERS_URL } from '../constants';
-import { apiSlice } from './apiSlice';
+import { USERS_URL } from "../constants";
+import { apiSlice } from "./apiSlice";
 
 const usersApiSlice = apiSlice.injectEndpoints({
-    endpoints: (builder) => ({
-        // 1. Add a `login` endpoint that performs a `POST` request to the `/users/login` URL.
-        login: builder.mutation({
-            query: ({ email, password }) => ({
-                url: `${USERS_URL}/login`,
-                method: 'POST',
-                body: { email, password },
-            }),
-        }),
-
-        // 2. Add a `logout` endpoint that performs a `POST` request to the `/users/logout` URL.
-        logout: builder.mutation({
-            query: () => ({
-                url: `${USERS_URL}/logout`,
-                method: 'POST',
-            }),
-        }),
-
-        // 3. Add a `register` endpoint that performs a `POST` request to the `/users` URL.
-        register: builder.mutation({
-            query: ({ name, email, password }) => ({
-                url: `${USERS_URL}`,
-                method: 'POST',
-                body: { name, email, password },
-            }),
-        }),
+  endpoints: (builder) => ({
+    // 1. Add a `login` endpoint that performs a `POST` request to the `/users/login` URL.
+    login: builder.mutation({
+      query: ({ email, password }) => ({
+        url: `${USERS_URL}/login`,
+        method: "POST",
+        body: { email, password },
+      }),
     }),
+
+    // 2. Add a `logout` endpoint that performs a `POST` request to the `/users/logout` URL.
+    logout: builder.mutation({
+      query: () => ({
+        url: `${USERS_URL}/logout`,
+        method: "POST",
+      }),
+    }),
+
+    // 3. Add a `register` endpoint that performs a `POST` request to the `/users` URL.
+    register: builder.mutation({
+      query: ({ name, email, password }) => ({
+        url: `${USERS_URL}`,
+        method: "POST",
+        body: { name, email, password },
+      }),
+    }),
+
+    // 4. Add a `getUser` endpoint that performs a `GET` request to the `/users/me` URL.
+    profile: builder.mutation({
+      query: (data) => ({
+        url: `${USERS_URL}/profile`,
+        method: "PUT",
+        body: data,
+      }),
+    }),
+  }),
 });
 
-export const { useLoginMutation, useLogoutMutation, useRegisterMutation } =
-    usersApiSlice;
+export const {
+  useLoginMutation,
+  useLogoutMutation,
+  useRegisterMutation,
+  useProfileMutation,
+} = usersApiSlice;
